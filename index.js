a9f2a6c8d281cfeca017e3fa359edf5696cec737
Ciaran Ashton <ciaranashton@me.com>
2018-07-22 22:46:55 +0100

    Update run funciton to take flags

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const generator = require('./lib/generate');
-const argv = require('yargs')
-    .array('actions')
-    .parse();
+const argv = require('yargs').argv;
+const { compact } = require('lodash');
 
-const start = () => {
-    const { module, actions } = argv;
-    
-	generator({
+const run = () => {
+    const { module, c, r, u, d } = argv;
+
+    generator({
         name: module,
-        actions,
-	});
+        actions: compact([
+            c && 'create',
+            r && 'fetch',
+            u && 'update',
+            d && 'delete',
+        ]),
+    });
 };
 
-start();
+run();
